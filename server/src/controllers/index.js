e17dfe8152d7fa738cfde0a0d0b89002883dba58
dang <hada.nguyen309@gmail.com>
2023-09-28 16:00:40 +0700

    rename controllers

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,13 +1,13 @@
 import AuthController from "./auth.controller.js";
 import CarController from "./car.controller.js";
-import InspectionResultController from "./inspectionResult.controller.js";
-import InspectionCriterionController from "./inspectionCriterion.controller.js";
+import ResultController from "./result.controller.js";
+import CriterionController from "./criterion.controller.js";
 import UserController from "./user.controller.js";
 
 export {
   AuthController,
   CarController,
-  InspectionResultController,
-  InspectionCriterionController,
+  ResultController,
+  CriterionController,
   UserController,
 };
